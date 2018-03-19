f2a13c2183d49624f5047e136a1ad011a8938096
Jassica <1648511816@qq.com>
2018-03-20 01:57:29 +0800

    first commit

---
+++ src/util/mock/againstData.js
@@ -0,0 +1,61 @@
+export default againstData=[{
+  id:1,
+  against:{
+    name:'青少年宫1',
+    time:new Date(8124938),
+    personList:['a','b','c','d','e','f'],
+    state:'start',//runing end
+    againstTable:[],
+    score:[10,20,13,12,12,10]
+  }
+},{
+  id:2,
+  against:{
+    name:'青少年宫2',
+    time:new Date(8524938),
+    personList:['a','b','c','d','e','f'],
+    state:'start',//runing end
+    againstTable:[],
+    score:[10,20,13,12,12,10]
+  }
+},{
+  id:3,
+  against:{
+    name:'青少年宫3',
+    time:new Date(8334938),
+    personList:['a','b','c','d','e','f'],
+    state:'start',//runing end
+    againstTable:[],
+    score:[10,20,13,12,12,10]
+  }
+},{
+  id:4,
+  against:{
+    name:'青少年宫4',
+    time:new Date(8344938),
+    personList:['a','b','c','d','e','f'],
+    state:'start',//runing end
+    againstTable:[],
+    score:[10,20,13,12,12,10]
+  }
+},{
+  id:5,
+  against:{
+    name:'青少年宫5',
+    time:new Date(8326938),
+    personList:['a','b','c','d','e','f'],
+    state:'start',//runing end
+    againstTable:[],
+    score:[10,20,13,12,12,10]
+  }
+},{
+  id:6,
+  against:{
+    name:'青少年宫6',
+    time:new Date(83249380),
+    personList:['a','b','c','d','e','f'],
+    state:'start',//runing end
+    againstTable:[],
+    score:[10,20,13,12,12,10]
+  }
+}]
---
+++ src/util/util.js
@@ -0,0 +1,44 @@
+const formatTime = date => {
+  const year = date.getFullYear()
+  const month = date.getMonth() + 1
+  const day = date.getDate()
+  const hour = date.getHours()
+  const minute = date.getMinutes()
+  const second = date.getSeconds()
+
+  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
+}
+
+const formatNumber = n => {
+  n = n.toString()
+  return n[1] ? n : '0' + n
+}
+
+module.exports = {
+  formatTime: formatTime
+}
+Array.prototype.remove = function (val) {
+  var index = this.indexOf(val);
+  if (index > -1) {
+    this.splice(index, 1);
+  }
+}
+Array.prototype.change = function (oldval,val) {
+  var index = this.indexOf(oldval);
+  if (index > -1) {
+    this[index]=val;
+  }
+}
+Array.prototype.numToString=function(stringArr){
+  for(let num of this){
+    if(Array.isArray(num)){
+      Array.prototype.numToString.call(num,stringArr)
+    }else{
+      if(num>stringArr.length){
+      }else{
+        this.change(num, stringArr[num - 1])
+        // this[this.indexOf(num)] = stringArr[num - 1]
+      }
+    }
+  }
+}

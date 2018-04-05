f5614c1b48504d210edcf595625ac01633965aa0
Jassica <1648511816@qq.com>
2018-04-05 18:59:15 +0800

    创建赛事成功

--- src/util/util.js
+++ src/util/util.js
@@ -1,4 +1,4 @@
-const formatTime = date => {
+const formatTime =(date,mark='/') => {
   const year = date.getFullYear()
   const month = date.getMonth() + 1
   const day = date.getDate()
@@ -6,7 +6,7 @@ const formatTime = date => {
   const minute = date.getMinutes()
   const second = date.getSeconds()
 
-  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
+  return [year, month, day].map(formatNumber).join(mark) + ' ' + [hour, minute, second].map(formatNumber).join(':')
 }
 
 const formatNumber = n => {

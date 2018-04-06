8e08a791a69ca14b39b3432ba1bc152d5fd26cf7
Jassica <1648511816@qq.com>
2018-04-06 20:18:37 +0800

    pr

--- src/util/util.js
+++ src/util/util.js
@@ -14,8 +14,55 @@ const formatNumber = n => {
   return n[1] ? n : '0' + n
 }
 
+const formateDate=(time)=>{
+  const days=['星期一','星期二','星期三','星期四','星期五','星期六','星期日']
+  const day = days[time.getDay()]
+  const month = time.getMonth() + 1
+  const date = time.getDate()
+  const hour = formatNumber(time.getHours())
+  const minute = formatNumber(time.getMinutes())
+  const joincode=[' ','月','日 ',':','']
+  return [day,month,date,hour,minute].reduce((string,curValue,curKey)=>{
+    return string+joincode[curKey-1]+curValue
+  })
+}
+const login=function(){
+  return new Promise((resolve,reject)=>{
+    console.log('进入login fn');
+    wx.login({
+      success:resolve,
+      fail:reject
+    })
+  })
+}
+const getUserInfo=function(){
+  return new Promise((resolve,reject)=>{
+    console.log('进入userInfo fn');
+    wx.getUserInfo({
+      url,
+      success:resolve,
+      fail:reject
+    })
+  })
+}
+const htr=function(url,method,data){
+  return new Promise((resolve,reject)=>{
+    console.log('进入htr fn');
+    wx.request({
+      url,
+      method,
+      data,
+      success:resolve,
+      fail:reject
+    })
+  })
+}
 module.exports = {
-  formatTime: formatTime
+  formatTime: formatTime,
+  formateDate:formateDate,
+  htr:htr,
+  login:login,
+  getUserInfo:getUserInfo,
 }
 Array.prototype.remove = function (val) {
   var index = this.indexOf(val);
